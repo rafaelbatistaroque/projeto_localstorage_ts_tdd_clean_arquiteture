10daf1739dea18c361292cbff0576ea7d176d4f5
Rafael Batista <maranata_rafa@hotmail.com>
2020-12-14 21:27:38 -0400

    garantir que LocalSavePurshases delete o chache antigo ao salvar

--- src/data/usecases/local_save_purchases.spec.ts
+++ src/data/usecases/local_save_purchases.spec.ts
@@ -1,19 +1,33 @@
 class LocalSavePurchases {
   constructor(private readonly cacheStore: ChacheStore) { }
+
+  async save(): Promise<void> {
+    this.cacheStore.delete();
+  }
 }
 
 interface ChacheStore {
-
+  delete(): void;
 }
 
 class ChacheStoreSpy implements ChacheStore {
   deleteCallsCount = 0;
+  delete(): void {
+    this.deleteCallsCount++;
+  }
 }
 
 describe("LocalSavePurchases", () => {
   test("Should not delete cache on sut.init", () => {
     const cacheStore = new ChacheStoreSpy();
-    const sut = new LocalSavePurchases(cacheStore);
+    new LocalSavePurchases(cacheStore);
     expect(cacheStore.deleteCallsCount).toBe(0);
+  });
+
+  test("Should not delete cache on sut.save", async () => {
+    const cacheStore = new ChacheStoreSpy();
+    const sut = new LocalSavePurchases(cacheStore);
+    await sut.save();
+    expect(cacheStore.deleteCallsCount).toBe(1);
   });
 });
